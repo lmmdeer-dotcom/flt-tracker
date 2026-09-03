667386d1f9d422bc7ce24234bf91e555ef9f708c
lmmdeer-dotcom <lmmdeer-dotcom@users.noreply.github.com>
2026-09-03 17:45:40 +0800

    v1.2: PWA 支持（manifest + service worker + 离线缓存 + 应用图标 + 动态主题色）

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE="flt-v1.2";
+const PRECACHE=["./","./index.html","./icons/icon-192.png","./icons/icon-512.png"];
+
+self.addEventListener("install",e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRECACHE)).catch(()=>{}));
+});
+
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
+  const sameOrigin=new URL(e.request.url).origin===self.location.origin;
+  e.respondWith(caches.match(e.request).then(hit=>{
+    if(hit)return hit;
+    const p=fetch(e.request);
+    if(!sameOrigin)return p;
+    return p.then(res=>{
+      if(res.ok){
+        const cp=res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,cp)).catch(()=>{});
+      }
+      return res;
+    });
+  }));
+});
