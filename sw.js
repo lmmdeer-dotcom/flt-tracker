10e73e1dc604b2b139d404a9b621fafb1b9939ac
lmmdeer-dotcom <lmmdeer-dotcom@users.noreply.github.com>
2026-09-07 17:54:58 +0800

    v1.9.3: 隐藏顶部 slogan 栏，同步状态 pill 固定右上角

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="flt-v1.9.2";
+const CACHE="flt-v1.9.3";
 const PRECACHE=["./","./index.html","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
 
 self.addEventListener("install",e=>{
